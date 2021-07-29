ebace765faadfbef54a8f951e27e096f60de34e0
chennanxiaogit1 <1821172805@qq.com>
2021-07-29 09:41:18 +0800

    差扩展

--- src/main.js
+++ src/main.js
@@ -4,10 +4,24 @@ import router from "./router/index.js"
 import store from "./store"
 
 import toast from "./components/common/toast"
+import fastClick from "fastclick"
+import VueLazyload from "vue-lazyload"
 
 Vue.config.productionTip = false
 //  安装插件
 Vue.use(toast)
+// 解决移动端300ms的延时
+fastClick.attach(document.body)
+// 图片懒加载
+Vue.use(VueLazyload)
+const loadimage = require('./assets/images/common/placeholder.png')
+
+Vue.use(VueLazyload, {
+  // preLoad: 1.3,
+  loading: loadimage,
+  // attempt: 1
+})
+
 Vue.prototype.$bus = new Vue()
 
 new Vue({
